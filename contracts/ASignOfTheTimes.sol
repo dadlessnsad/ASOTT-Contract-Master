3f394647d2ba3a96bea5f52cd4182a5683270dd9
dadlessnsad <tbodnar85@gmail.com>
2022-03-29 18:37:04 -0600

    inital commint

---
+++ contracts/ASignOfTheTimes.sol
@@ -0,0 +1,148 @@
+//SPDX License-Identifier: UNLICENSED
+
+/// @title A Sign Of The Times
+/// @author @GBERGPHOTO
+/// @notice ASOTT is a photographic diary by Gregory Berg
+/// @dev @0xORPHAN x @TRANSMENTAL
+/*
+ █████╗     ███████╗██╗ ██████╗ ███╗   ██╗        
+██╔══██╗    ██╔════╝██║██╔════╝ ████╗  ██║        
+███████║    ███████╗██║██║  ███╗██╔██╗ ██║        
+██╔══██║    ╚════██║██║██║   ██║██║╚██╗██║        
+██║  ██║    ███████║██║╚██████╔╝██║ ╚████║        
+╚═╝  ╚═╝    ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝        
+ ██████╗ ███████╗    ████████╗██╗  ██╗███████╗    
+██╔═══██╗██╔════╝    ╚══██╔══╝██║  ██║██╔════╝    
+██║   ██║█████╗         ██║   ███████║█████╗      
+██║   ██║██╔══╝         ██║   ██╔══██║██╔══╝      
+╚██████╔╝██║            ██║   ██║  ██║███████╗    
+ ╚═════╝ ╚═╝            ╚═╝   ╚═╝  ╚═╝╚══════╝    
+████████╗██╗███╗   ███╗███████╗███████╗           
+╚══██╔══╝██║████╗ ████║██╔════╝██╔════╝           
+   ██║   ██║██╔████╔██║█████╗  ███████╗           
+   ██║   ██║██║╚██╔╝██║██╔══╝  ╚════██║           
+   ██║   ██║██║ ╚═╝ ██║███████╗███████║           
+   ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝                                                                                      
+*/   
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+contract ASignOfTheTimes is ERC721A, ReentrancyGuard, Ownable {
+
+    string public baseURI;
+
+    address payable public GregoryBerg = payable(0x33602B325F169741662cD33C3F693Eee8dbD20D5);
+
+    bool public paused = true;
+
+    uint public nftPerAddress = 1;
+    uint public maxSupply = 100;
+    uint256 cost = 0.15 ether;
+
+    mapping(address => uint256) public addressMintedBalance;
+
+    constructor(
+        string memory _initBaseURI
+    )ERC721A("A Sign Of The Times", "ASSOT") 
+    {
+       baseURI= _initBaseURI;
+    }
+
+    modifier unPaused() {
+        require(!paused, 'Contract paused');
+        _;
+    }
+
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, 'The caller is another contract.');
+        _;
+    }
+
+    modifier isCorrectPayment(uint256 price, uint256 _amount) {
+        require(
+            cost * _amount >= msg.value,
+            "Incorrect ETH value sent"
+        );
+        _;
+    }
+
+    function developNFT(
+        uint256 _amount
+    ) 
+        public 
+        payable 
+        unPaused() 
+        callerIsUser() 
+        isCorrectPayment(cost, _amount)
+    {
+        uint supply = totalSupply();
+        require((_amount + supply) <= maxSupply, "Sold Out");
+        require(_amount + addressMintedBalance[msg.sender] <= nftPerAddress, "one NFT Per Address");
+
+        for (uint256 i = 1; i <= _amount; i++) {
+            addressMintedBalance[msg.sender]++;
+            _safeMint(msg.sender, _amount);
+        }
+    }
+
+
+
+    function setBaseURI(string memory _newBaseURI) external onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function togglePaused(bool _state) external onlyOwner {
+        paused = _state;
+    }
+
+    function setMintCost(uint256 _cost) external onlyOwner {
+        cost = _cost;
+    }
+
+    function setNftPerAddress(uint _nftPerAddress) external onlyOwner {
+        nftPerAddress = _nftPerAddress;
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = GregoryBerg.call{value: address(this).balance}("");
+        require(success, "Failed to send to lead.");
+    }
+
+    function tokenURI(uint256 _tokenId) 
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId), "Token does not exist.");
+        return string(abi.encodePacked(baseURI, Strings.toString(_tokenId),".json"));
+    }
+}
+
+
+/*
+ █████╗     ███████╗██╗ ██████╗ ███╗   ██╗        
+██╔══██╗    ██╔════╝██║██╔════╝ ████╗  ██║  
+███████║    ███████╗██║██║  ███╗██╔██╗ ██║        
+██╔══██║    ╚════██║██║██║   ██║██║╚██╗██║        
+██║  ██║    ███████║██║╚██████╔╝██║ ╚████║        
+╚═╝  ╚═╝    ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝        
+ ██████╗ ███████╗    ████████╗██╗  ██╗███████╗    
+██╔═══██╗██╔════╝    ╚══██╔══╝██║  ██║██╔════╝    
+██║   ██║█████╗         ██║   ███████║█████╗      
+██║   ██║██╔══╝         ██║   ██╔══██║██╔══╝      
+╚██████╔╝██║            ██║   ██║  ██║███████╗    
+ ╚═════╝ ╚═╝            ╚═╝   ╚═╝  ╚═╝╚══════╝    
+████████╗██╗███╗   ███╗███████╗███████╗           
+╚══██╔══╝██║████╗ ████║██╔════╝██╔════╝           
+   ██║   ██║██╔████╔██║█████╗  ███████╗           
+   ██║   ██║██║╚██╔╝██║██╔══╝  ╚════██║           
+   ██║   ██║██║ ╚═╝ ██║███████╗███████║           
+   ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝                                                                                      
+*/    
---
+++ hardhat.config.js
@@ -0,0 +1,53 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require('hardhat-contract-sizer');
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+// require("@nomiclabs/hardhat-etherscan");
+const { expect } = require("chai");
+const { DEPLOYER_PRIVATE_KEY, INFURA_PROJECT_ID, ETHERSCAN_API_KEY } = process.env;  
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    // rinkeby: {
+    //   url: INFURA_PROJECT_ID,
+    //   accounts: [`${DEPLOYER_PRIVATE_KEY}`]
+    // }
+  },
+  solidity: "0.8.7",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 999
+    }
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+  },
+    gasReporter: {
+      currency: 'USD',
+      gasPrice: 47,
+      coinmarketcap: "ed130847-6c1e-4071-b79d-0e037d5df036"
+    },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
